55beb4fbdad8d3b338c403e94de7806a1eca6f53
IvanNugiban <aa1245541@gmail.com>
2023-05-19 22:15:42 +0300

    Added responsive design for convert section

--- src/components/ConvertSection.tsx
+++ src/components/ConvertSection.tsx
@@ -6,12 +6,12 @@ const ConvertSection : React.FC = () => {
 
 return (
     <section className="bg-neutral-200">
-        <div className="container mx-auto px-28 pt-12 pb-24">
+        <div className="container mx-auto px-4 lg:px-28 pt-12 pb-24">
             <h1 className="text-5xl text-neutral-700 font-bold mb-4 text-center">Convert your currency</h1>
             <h2 className="text-2xl text-neutral-600  text-center">Free. Fast. Up-to-date.</h2>
 
             <div className="bg-white border-r-6 rounded-lg p-10  shadow-lg mt-20">
-                <div className="flex items-center justify-between gap-10">
+                <div className="flex flex-col lg:flex-row items-center justify-between gap-2 lg:gap-10">
                     <ConvertableItem text="Convertible currency"/>
 
                     <img src={ReverseArrows} className="h-10 pt-4" alt=""/>
